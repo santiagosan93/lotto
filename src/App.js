bc7bf333994ebbd1dd35b025c799d220c0e31b8d
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2020-03-23 19:59:59 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,64 +3,11 @@ import Lotto from './components/lotto'
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      numbers: this.pullNumbers(60, 6),
-      dailey: this.pullNumbers(20,4)
-    }
-    this.pullNumbers = this.pullNumbers.bind(this)
-    this.generateNormal = this.generateNormal.bind(this)
-    this.generateDaily = this.generateDaily.bind(this)
-  }
-
-  pullNumbers(max, ballQuantity) {
-    // Fill nums of 6 and of 4
-    const numbers = new Array(max).fill().map((number,index) => index + 1)
-    const selectedNumbers = []
-    while(selectedNumbers.length < ballQuantity) {
-      const index = Math.floor(Math.random() * max)
-      const pulledNumber = numbers.splice(index, 1)[0]
-      selectedNumbers.push(pulledNumber)
-      max -= 1
-    }
-    return selectedNumbers
-  }
-
-  generateNormal(max, ballQuantity) {
-    const numbers = new Array(max).fill().map((number,index) => index + 1)
-    const selectedNumbers = []
-    while(selectedNumbers.length < ballQuantity) {
-      const index = Math.floor(Math.random() * max)
-      const pulledNumber = numbers.splice(index, 1)[0]
-      selectedNumbers.push(pulledNumber)
-      max -= 1
-    }
-    this.setState({
-      numbers: selectedNumbers
-    })
-  }
-
-   generateDaily(max, ballQuantity) {
-    const numbers = new Array(max).fill().map((number,index) => index + 1)
-    const selectedNumbers = []
-    while(selectedNumbers.length < ballQuantity) {
-      const index = Math.floor(Math.random() * max)
-      const pulledNumber = numbers.splice(index, 1)[0]
-      selectedNumbers.push(pulledNumber)
-      max -= 1
-    }
-    this.setState({
-      dailey: selectedNumbers
-    })
-  }
-
-
   render() {
-    console.log(this.state)
     return (
       <div className='App'>
-        <Lotto numbers={this.state.numbers} dailey={this.state.dailey} generateNormal={this.generateNormal} generateDaily={this.generateDaily}/>
+        <Lotto title='Lotto Game!' maxNumber={60} maxBalls={6}/>
+        <Lotto title='Mini Daily' maxNumber={20} maxBalls={4}/>
       </div>
     )
   }
--- src/components/lotto.js
+++ src/components/lotto.js
@@ -1,27 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Ball from './ball'
 
-const Lotto = (props) => {
-  const normalLotto = props.numbers.map((number, index) => {return <Ball number={number} key={index}/>})
-  const daileyLotto = props.dailey.map((number,index) => {return <Ball number={number} key={index}/>})
-  return (
-    <div className='lotto'>
-      <div className='big-price'>
-        <h2>Lotto Game!</h2>
-        <div className='ball-holder'>
-          {normalLotto}
+class Lotto extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      maxBalls: this.props.maxBalls,
+      maxNumber: this.props.maxNumber,
+      numbers: []
+    }
+    this.pullNumbers = this.pullNumbers.bind(this)
+  }
+
+
+  pullNumbers() {
+    // Fill nums of 6 and of 4
+    let max = this.state.maxNumber
+    const numbers = new Array(this.state.maxNumber).fill().map((number,index) => index + 1)
+    const selectedNumbers = []
+    while(selectedNumbers.length < this.state.maxBalls) {
+      const index = Math.floor(Math.random() * max)
+      const pulledNumber = numbers.splice(index, 1)[0]
+      selectedNumbers.push(pulledNumber)
+      max -= 1
+    }
+    this.setState({
+      numbers: selectedNumbers
+    })
+  }
+
+  render() {
+    const normalLotto = this.state.numbers.map((number, index) => {return <Ball number={number} key={index}/>})
+    return (
+      <div className='lotto'>
+        <div className='big-price'>
+          <h2>{this.props.title}</h2>
+          <div className='ball-holder'>
+            {normalLotto}
+          </div>
+          <button onClick={this.pullNumbers}>Generate</button>
         </div>
-        <button onClick={e => props.generateNormal(60,6)}>Generate</button>
       </div>
-      <div className='small-price'>
-        <h2>dailey Game!</h2>
-        <div className='ball-holder'>
-          {daileyLotto}
-        </div>
-        <button onClick={e => props.generateDaily(20,4)}>Generate</button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Lotto
